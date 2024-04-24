6bb2f073aa821a477a636086485162e6f1330365
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-04-24 18:27:39 +0200

    Fed v1 - @external and extends

---
+++ src/test-cases/fed1-external-extends/data.ts
@@ -0,0 +1,12 @@
+export const users = [
+  {
+    id: "u1",
+    name: "u1-name",
+    nickname: "u1-nickname",
+  },
+  {
+    id: "u2",
+    name: "u2-name",
+    nickname: "u2-nickname",
+  },
+];
