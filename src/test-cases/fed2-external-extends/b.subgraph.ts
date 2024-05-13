33521b01ce2a823e0174598382b5e8cf90e0a334
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-05-13 15:40:08 +0200

    Cover combination of external and extension (#30)
    Adds a variation of `@external`, `extends User` and `type User @extends`
    for both Fed v1 and v2

---
+++ src/test-cases/fed2-external-extends/b.subgraph.ts
@@ -0,0 +1,51 @@
+import { createSubgraph } from "../../subgraph";
+import { users } from "./data";
+
+export default createSubgraph("b", {
+  typeDefs: /* GraphQL */ `
+    extend schema
+      @link(url: "https://specs.apollo.dev/federation/v2.3", import: ["@key"])
+
+    type Query {
+      userById(id: ID): User
+    }
+
+    type User @key(fields: "id") {
+      id: ID!
+      name: String!
+      nickname: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      userById: (_: {}, { id }: { id: string }) => {
+        const user = users.find((u) => u.id === id);
+
+        if (!user) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          nickname: user.nickname,
+        };
+      },
+    },
+    User: {
+      __resolveReference: (key: { id: string }) => {
+        const user = users.find((u) => u.id === key.id);
+
+        if (!user) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          nickname: user.nickname,
+        };
+      },
+    },
+  },
+});
