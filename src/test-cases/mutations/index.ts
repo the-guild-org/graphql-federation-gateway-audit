8fa3dec5aef5c54b06f34a53f1ada38f4dbf32a7
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-05-16 11:05:03 +0200

    Ensure mutations are running in correct order

--- src/test-cases/mutations/index.ts
+++ src/test-cases/mutations/index.ts
@@ -1,6 +1,7 @@
 import { serve } from "../../supergraph";
 import a from "./a.subgraph";
 import b from "./b.subgraph";
+import c from "./c.subgraph";
 import test from "./test";
 
-export default serve("mutations", [a, b], test);
+export default serve("mutations", [a, b, c], test);
--- src/test-cases/mutations/test.ts
+++ src/test-cases/mutations/test.ts
@@ -1,50 +1,71 @@
 import { createTest } from "../../test";
 
-export default [
-  createTest(
-    /* GraphQL */ `
+export default () => {
+  const randomId = Math.random().toString(16).substr(2);
+
+  return [
+    createTest(
+      /* GraphQL */ `
+        mutation {
+          addProduct(input: { name: "new", price: 599.99 }) {
+            name
+            price
+            isExpensive
+            isAvailable
+          }
+        }
+      `,
+      {
+        data: {
+          addProduct: {
+            name: "new",
+            price: 599.99,
+            isExpensive: true,
+            isAvailable: true,
+          },
+        },
+      }
+    ),
+    createTest(
+      /* GraphQL */ `
+        query {
+          product(id: "p1") {
+            id
+            name
+            price
+            isExpensive
+            isAvailable
+          }
+        }
+      `,
+      {
+        data: {
+          product: {
+            id: "p1",
+            name: "p1-name",
+            price: 9.99,
+            isExpensive: false,
+            isAvailable: true,
+          },
+        },
+      }
+    ),
+    // Test correct order of execution
+    createTest(
+      /* GraphQL */ `
       mutation {
-        addProduct(input: { name: "new", price: 599.99 }) {
-          name
-          price
-          isExpensive
-          isAvailable
-        }
+        five: add(num: 5, requestId: "${randomId}")
+        seven: add(num: 2, requestId: "${randomId}")
+        final: delete(requestId: "${randomId}")
       }
     `,
-    {
-      data: {
-        addProduct: {
-          name: "new",
-          price: 599.99,
-          isExpensive: true,
-          isAvailable: true,
+      {
+        data: {
+          five: 5,
+          seven: 7,
+          final: 7,
         },
-      },
-    }
-  ),
-  createTest(
-    /* GraphQL */ `
-      query {
-        product(id: "p1") {
-          id
-          name
-          price
-          isExpensive
-          isAvailable
-        }
       }
-    `,
-    {
-      data: {
-        product: {
-          id: "p1",
-          name: "p1-name",
-          price: 9.99,
-          isExpensive: false,
-          isAvailable: true,
-        },
-      },
-    }
-  ),
-];
+    ),
+  ];
+};
