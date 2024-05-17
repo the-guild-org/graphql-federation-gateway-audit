452430b15d9a11702bef18dec5ed985ae373b3e7
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-05-17 10:33:54 +0200

    non-resolvable-interface-object

--- src/test-cases/non-resolvable-interface-object/test.ts
+++ src/test-cases/non-resolvable-interface-object/test.ts
@@ -22,6 +22,10 @@ export default [
     QueryPlan {
       Fetch(service: "b") {
         {
+          # NOTE
+          # Even though Node in subgraph B is an interface object,
+          # there is no type condition in the query,
+          # so "id" and "field" fields can be resolved directly.
           b {
             id
             field
@@ -45,6 +49,9 @@ export default [
       },
     },
     /* GraphQL */ `
+    # NOTE
+    # Query.a is only available in subgraph A.
+    # Query.a resolves Node interface, but none of the object types in subgraph A implement Node.
     QueryPlan {}
     `
   ),
@@ -67,6 +74,10 @@ export default [
     QueryPlan {
       Fetch(service: "b") {
         {
+          # NOTE
+          # Even though Node in subgraph B is an interface object,
+          # there is no type condition in the query,
+          # so "id" field can be resolved directly.
           b {
             id
           }
@@ -93,6 +104,15 @@ export default [
     QueryPlan {
       Fetch(service: "a") {
         {
+          # NOTE
+          # Query.a is only available in subgraph A.
+          # Query.a resolves Node interface,
+          # but none of the object types in subgraph A implement Node.
+          # Asking for "id" field is not possible.
+          # It's interesting that the query planner doesn't make an empty query to subgraph A.
+          # It clearly knows that the query is impossible to resolve.
+          # I would imagine that the request would be { a { __typename } },
+          # just like in the previous test case where we fetched { a { field } }.
           a {
             __typename
             id
