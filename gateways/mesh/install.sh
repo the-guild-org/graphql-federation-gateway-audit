bea55ac4b8261838d6d29be904d0a2eff7a21460
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-07-23 18:53:01 +0300

    Bump

--- gateways/mesh/install.sh
+++ gateways/mesh/install.sh
@@ -1,2 +1,2 @@
 # It's just to install dependencies and make it ready for `run.sh` script
-npx --yes @graphql-mesh/serve-cli@0.8.1-alpha-20240723143736-4a9162a84a21477798641ad5f29cbed0b9943477 mesh-serve --help
+npx --yes @graphql-mesh/serve-cli@0.8.1-alpha-20240723155057-eb04c5aec6bd3edd0c788e7c5b7a402edc5623c5 mesh-serve --help
