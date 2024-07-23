ba40a39711f3e95f1d9ef0038b9a284b8ba1603c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-07-23 19:09:45 +0300

    Use stable

--- gateways/mesh/run.sh
+++ gateways/mesh/run.sh
@@ -1,3 +1,3 @@
 echo "Downloading supergraph.graphql from $1"
 curl -L $1 > supergraph.graphql
-npx --yes @graphql-mesh/serve-cli@0.8.1-alpha-20240723155057-eb04c5aec6bd3edd0c788e7c5b7a402edc5623c5 mesh-serve --fork 1
+npx --yes @graphql-mesh/serve-cli@0.8.1 mesh-serve --fork 1
