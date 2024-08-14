c6f5483e035b2e30088bfea3ab80af3749b30e08
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-08-14 15:27:38 +0200

    Upgrade Mesh to v0.10.0 (#63)

--- gateways/mesh/run.sh
+++ gateways/mesh/run.sh
@@ -1,2 +1,2 @@
 npm start supergraph -- --cwd ./gateways/mesh --test $1
-npx --yes @graphql-mesh/serve-cli@0.9.0 mesh-serve --port 4000 --fork 1 --supergraph supergraph.graphql
+npx --yes @graphql-mesh/serve-cli@0.10.0 supergraph supergraph.graphql --port 4000 --fork 1
