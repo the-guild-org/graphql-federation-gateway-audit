01f4b80c83248e8591bf10a1beab4465e189c4ef
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-08-26 13:05:55 +0300

    Try alpha

--- gateways/mesh/install.sh
+++ gateways/mesh/install.sh
@@ -1,2 +1,2 @@
 # It's just to install dependencies and make it ready for `run.sh` script
-npx --yes @graphql-mesh/serve-cli@0.11.4 --version
+npx --yes @graphql-mesh/serve-cli@0.11.5-alpha-20240826100406-67e75ba8acf7aca0d88205e2c50d4bc8003bb7cb --version
--- gateways/mesh/run.sh
+++ gateways/mesh/run.sh
@@ -1,2 +1,2 @@
 npm start supergraph -- --cwd ./gateways/mesh --test $1
-npx --yes @graphql-mesh/serve-cli@0.11.4 supergraph supergraph.graphql --port 4000 --fork 1
+npx --yes @graphql-mesh/serve-cli@0.11.5-alpha-20240826100406-67e75ba8acf7aca0d88205e2c50d4bc8003bb7cb supergraph supergraph.graphql --port 4000 --fork 1
