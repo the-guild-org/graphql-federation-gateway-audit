65bd51f50f111feb2b54cbe874afb3e6492b4e87
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-08-29 14:06:46 +0300

    Use `@graphql-hive/gateway`

--- gateways/hive/run.sh
+++ gateways/hive/run.sh
@@ -1,2 +1,2 @@
 npm start supergraph -- --cwd ./gateways/hive --test $1
-npx --yes @graphql-mesh/serve-cli@0.11.5 supergraph supergraph.graphql --port 4000 --fork 1
+npx --yes @graphql-hive/gateway@0.1.0 supergraph supergraph.graphql --port 4000 --fork 1
