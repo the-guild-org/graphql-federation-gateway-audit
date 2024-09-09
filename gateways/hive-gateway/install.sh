79f5dbf04c640937860a4fe62453ba7c954e8607
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-09-09 14:39:06 -0700

    bump version of hive gateway

--- gateways/hive-gateway/install.sh
+++ gateways/hive-gateway/install.sh
@@ -1,2 +1,2 @@
 # It's just to install dependencies and make it ready for `run.sh` script
-npx --yes @graphql-hive/gateway@0.1.1 --version
+npx --yes @graphql-hive/gateway@0.1.4 --version
--- gateways/hive-gateway/run.sh
+++ gateways/hive-gateway/run.sh
@@ -1,2 +1,2 @@
 npm start supergraph -- --cwd ./gateways/hive-gateway --test $1
-npx --yes @graphql-hive/gateway@0.1.1 supergraph supergraph.graphql --port 4000 --fork 1
+npx --yes @graphql-hive/gateway@0.1.4 supergraph supergraph.graphql --port 4000 --fork 1
