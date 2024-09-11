b6f6c538e9ea45475d209e09262f1a21a61f6a8d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-09-11 04:43:24 -0700

    Use Binary of Hive Gateway

--- gateways/hive-gateway/install.sh
+++ gateways/hive-gateway/install.sh
@@ -1,2 +1,2 @@
 # It's just to install dependencies and make it ready for `run.sh` script
-npx --yes @graphql-hive/gateway@0.1.4 --version
+curl -sSL https://graphql-hive.com/install-gateway.sh | sh
